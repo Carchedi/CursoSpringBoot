06243ee30d4a84685369c260f4fade0c2ee50067
Carchedi <carchedi.ufjf@gmail.com>
2020-06-05 22:45:12 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/carchedi/cursoSpringBoot/resources/CategoriaResource.java
+++ src/main/java/com/carchedi/cursoSpringBoot/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.carchedi.cursoSpringBoot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.carchedi.cursoSpringBoot.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String Listar() {
-		return "Rest funcionando!!";
+	public List<Categoria> Listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
+	
 	}
 } 
