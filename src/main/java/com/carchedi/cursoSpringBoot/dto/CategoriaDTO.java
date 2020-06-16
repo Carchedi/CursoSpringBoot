e648adc50d0c4cf1760b78e156c6a0a273faee55
Carchedi <carchedi.ufjf@gmail.com>
2020-06-16 18:41:14 -0300

    Validacao Sintatica com BeamValidation

--- src/main/java/com/carchedi/cursoSpringBoot/dto/CategoriaDTO.java
+++ src/main/java/com/carchedi/cursoSpringBoot/dto/CategoriaDTO.java
@@ -2,14 +2,19 @@ package com.carchedi.cursoSpringBoot.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import com.carchedi.cursoSpringBoot.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	  
 	private static final long serialVersionUID = 1L;
 	
-
 	private Integer id;
+ 
+	@NotEmpty(message="Preencimento obrigatório")
+	@Size(min=5, max=80, message="O tamanho precisa estar entre 5 e 80 caracteres")	
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/carchedi/cursoSpringBoot/services/CategoriaService.java
+++ src/main/java/com/carchedi/cursoSpringBoot/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.carchedi.cursoSpringBoot.domain.Categoria;
+import com.carchedi.cursoSpringBoot.dto.CategoriaDTO;
 import com.carchedi.cursoSpringBoot.repositories.CategoriaRepository;
 import com.carchedi.cursoSpringBoot.services.exceptions.DataIntegrityException;
 import com.carchedi.cursoSpringBoot.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page,  linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO obj) {
+		return new Categoria(obj.getId(), obj.getNome());
 	}
 }
