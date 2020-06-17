1964fd5e5006c3206f758a5a0d3fbfedfa2a4509
Carchedi <carchedi.ufjf@gmail.com>
2020-06-17 20:49:58 -0300

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/com/carchedi/cursoSpringBoot/dto/ClienteDTO.java
@@ -0,0 +1,58 @@
+package com.carchedi.cursoSpringBoot.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.carchedi.cursoSpringBoot.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+	  
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+
+	@NotEmpty(message="Preencimento obrigatório")
+	@Size(min=5, max=120, message="O tamanho precisa estar entre 5 e 120 caracteres")	
+	private String nome;
+	
+	@NotEmpty(message="Preencimento obrigatório")
+	@Email(message="Email inválido")
+	private String email;
+	
+	public ClienteDTO() {
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
